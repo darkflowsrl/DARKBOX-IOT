f9aa0dd3a914dcbeea99cffd99aeb7895c741f14
giuliano-crenna <59456310+giulicrenna@users.noreply.github.com>
2022-06-16 19:15:30 -0300

    16/06/2022
    Arreglar!!!

--- src/esp_32_GPIO.cpp
+++ src/esp_32_GPIO.cpp
@@ -1,14 +1,11 @@
-//mosquitto_pub -h <host> -t <"topic"> -m <"message">
-//mosquitto_sub -h <host> -t <"topic"> 
-//mosquitto_pub -h mqtt.darkflow.com.ar -t giuli/data
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
+#include <jsonizer.h>
 
 #include <Arduino.h>
-#include <ArduinoJson.h>
 #include <OneWire.h>
 #include <DHT.h>
 #include <WiFi.h>
@@ -16,27 +13,19 @@
 #include <DallasTemperature.h>
 #include <PubSubClient.h>
 
+//mosquitto_pub -h <host> -t <"topic"> -m <"message">
+//mosquitto_sub -h <host> -t <"topic"> 
+//mosquitto_pub -h mqtt.darkflow.com.ar -t giuli/data -m 
+
 #define humiditySensor_PIN 23 // GPIO pins from the ESP-32
 #define tempSensor1_PIN 32
 #define DHTTYPE  DHT21 //Define the type of DHT sensor
 
 using namespace std;
 
-StaticJsonDocument<1024> config;
-char json_[] = "{\"ssid\":\"Chopin\",\"wifiPassword\": \"Euler27182\",\"host\": \"mqtt.darkflow.com.ar\",\"root_topic_subscribe\": \"giuli/testing\",\"root_topic_publish\": \"giuli/data\",\"userName\": \" \",\"password\": \" \",\"port\":1883,\"mailSender\": \"giulicrenna@outlook.com\",\"mailReceiver\": \"kirchhoff2002\",\"mailPassword\": \"giuli_crenna@gmail.com\",\"smtpServer\": \"smtp.office365.com\",\"smtpPort\":587}";
-DeserializationError error = deserializeJson(config, json_); // deserialize() allows a JSON data and an array that contains the JSON to deserialize 
-
-
-const char* ssid = config["ssid"];
-const char* wifiPassword = config["wifiPassword"];
-const char* host = config["host"];
-const char* root_topic_subscribe = config["root_topic_subscribe"];
-const char* root_topic_publish = config["root_topic_publish"];
-const char* userName = config["userName"];
-const char* password = config["password"];
-const int port = config["port"];  
-String stringifiedJSON = "{"; //This variable will be used by the JSONIZER, here will the "data" be inserted.
-
+void callback(char* topic, byte* payload, unsigned int lenght);
+void reconnect();
+void setup_wifi();
 
 //Constructor for the sensors, the wifi and the MQTT Object
 OneWire oneWire(tempSensor1_PIN); 
@@ -44,15 +33,24 @@ DallasTemperature DS18B20(&oneWire);
 DHT humiditySensor(humiditySensor_PIN, DHTTYPE);
 WiFiClient espClient; 
 PubSubClient client(espClient);
+JSONIZER JSONsession;
+
+const string json = JSONsession.readFileIntoString(".\\config.json");
+vector<string> configuration = JSONsession.toVECTOR(json);
+
+const char* ssid = configuration[1].c_str();
+const char* wifiPassword = configuration[3].c_str();
+const char* host = configuration[5].c_str();
+const char* root_topic_subscribe = configuration[7].c_str();
+const char* root_topic_publish = configuration[9].c_str();
+const char* userName = configuration[11].c_str();
+const char* password = configuration[13].c_str();
+const int port = stoi(configuration[15]);
 
 int sensorsCount = 0;
 float tempC;
-
 char message[100];
-void callback(char* topic, byte* payload, unsigned int lenght);
-void reconnect();
-void setup_wifi();
-void JSONIZER(String data0, float data1, bool doEnd);
+vector<string> tempData;
 
 class terminalMessages{
   private:
@@ -92,28 +90,25 @@ void loop() {
   DS18B20.requestTemperatures();
   for(int n = 0; n < sensorsCount; n++){
     String sensorNumber = "Sensor" + String(n);
-    if(n < sensorsCount-1){
-      JSONIZER(sensorNumber, DS18B20.getTempCByIndex(n), false);
-    }else{
-      JSONIZER(sensorNumber, DS18B20.getTempCByIndex(n), false);
+    if(n < sensorsCount){
+      tempData.push_back(sensorNumber.c_str());
+      tempData.push_back(String(DS18B20.getTempCByIndex(n), 3).c_str());   
     }
   }
   //Humidity data
   float ambientHumidity = humiditySensor.readTemperature();
-  JSONIZER("Humidity", ambientHumidity, true);
+  tempData.push_back("Humidity");
+  tempData.push_back(String(ambientHumidity, 3).c_str());   
   //MQTT
   if(!client.connected()){
     reconnect();
   }
   if(client.connected()){
-    Serial.println(stringifiedJSON);
-    stringifiedJSON.toCharArray(message, 100);
-    client.publish(root_topic_publish, message);
+    string data = JSONsession.toSJSON(tempData);
+    client.publish(root_topic_publish, data.c_str());
     delay(2000);
   }
   client.loop();
-  // {"temp0":x,"temp1:x,"temp2":x} 
-  stringifiedJSON = "{";
 }
 //function> setup_wifi: None -> int
 void setup_wifi(){
@@ -168,14 +163,3 @@ void reconnect(){
   }
 }
 
-//function> JSONIZER: Any, Any, bool -> void
-void JSONIZER(String data0, float data1, bool doEnd){
-  if(doEnd == false){
-    String stringStream = "\"" + data0 + "\" : \"" + data1 + "\", ";   
-    stringifiedJSON += stringStream;
-  } 
-  if(doEnd == true){
-    String stringStream = "\"" + data0 + "\" : \"" + data1 + "\"} ";
-    stringifiedJSON += stringStream;
-  }
-}
